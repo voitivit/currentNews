3116b9094d851eecc8fbb037369c99d5f5c77e60
Lewis <battlemasa@gmail.com>
2022-06-14 22:58:34 +0300

    add develop branch, news api key added

---
+++ currentNews/currentNews/Services/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  currentNews
+//
+//  Created by Lewis on 14.06.2022.
+//
+
+import Foundation
+
+struct NetworkService {
+    
+    static let apiKey = "9ae8d7e609144345a643c16cd2425af6"
+}
