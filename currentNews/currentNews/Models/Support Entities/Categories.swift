7765ac23fcc82e2d527e33ab1b39e5b4274954e0
Lewis <battlemasa@gmail.com>
2022-06-18 23:27:11 +0300

    add basic network methods

---
+++ currentNews/currentNews/Models/Support Entities/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  currentNews
+//
+//  Created by Lewis on 18.06.2022.
+//
+
+import Foundation
+
+enum Categories: String, CaseIterable {
+    case business = "business"
+    case entertainment = "entertainment"
+    case general = "general"
+    case health = "health"
+    case science = "science"
+    case sports = "sports"
+    case technology = "technology"
+}
---
+++ currentNews/currentNews/Models/Support Entities/NewsLanguages.swift
@@ -0,0 +1,26 @@
+//
+//  NewsLanguages.swift
+//  currentNews
+//
+//  Created by Lewis on 18.06.2022.
+//
+
+import Foundation
+
+enum NewsLanguages: String {
+    case ar = "Arabic"
+    case de = "Deutsch"
+    case en = "English"
+    case es = "Spanish"
+    case fr = "French"
+    case he = "Hebrew"
+    case it = "Italian"
+    case nl = "Dutch"
+    case no = "//"
+    case pt = "Portuguese"
+    case ru = "Russian"
+    case sv = "Swedish"
+    case ud = "///"
+    case zh = "Chinese"
+}
+ 
